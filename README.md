5763cea64d0f4772edc8ad9ad69ad951a90acab5
pandaant <apandaant@gmail.com>
2014-06-24 19:00:41 +0200

    initial import

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
 huffmanrb
 =========
 
-Implements a minimal Huffman encoder/decoder in ruby
+Implements a minimal Huffman encoder/decoder in ruby.
+This Project was created for the data compression course at FH-Aachen.
+
+Example usage:
+========
+Create input to encode. 50% a's, 33.33% b's, 16.66% c's.
+    ruby -e "puts %w[a a a b b c].map{ |c| [c]*10000 }.flatten.shuffle.join" > testinput
+
+Compress input to file
+    ./encoder < testinput > enc.testinput
+
+Decompress encoded file to file
+    ./decoder < enc.testinput > dec.testinput
+
+Compare file sizes
+    du -h testinput enc.testinput
+
+Possible output:
+    60K testinput
+    16K enc.testinput
+
+Compare md5 checksums of original and the decoded file
+    md5sum testinput dec.testinput
+
+Possible output:
+    7de2762f5bd59a7f95796d3fa43b23d1  testinput
+    7de2762f5bd59a7f95796d3fa43b23d1  dec.testinput
---
+++ node.rb
@@ -0,0 +1,40 @@
+require 'json'
+
+#
+# represents a node in the huffmann tree. 
+#
+# left/right    children
+# value         leafs store the bytevalue there
+# freq          the frequency of that node
+# 
+
+class Node < Struct.new :left, :right, :value, :freq
+    # generates the tree from an nested array
+    def self.from_s s
+        s.kind_of?(Array)? 
+            Node[*s.map{|e|Node.from_s e},nil,0] : Node[nil,nil,s,0]
+    end
+
+    # generates a lookuptable for the generated
+    # tree construct. left branch is associated
+    # with 1 and right with 0.
+    def table p = {}, tail = []
+        p[value] = tail.reverse.join unless value.nil?
+        unless leaf?
+            left.table  p, tail + [1] 
+            right.table p, tail + [0]
+        end;p
+    end
+
+    def to_a # table in condensed format
+        leaf?? value : [left.to_a,right.to_a]
+    end
+
+    def leaf?
+        [left, right].any? &:nil?
+    end
+
+    def <=> n 
+        freq <=> n.freq
+    end
+end
